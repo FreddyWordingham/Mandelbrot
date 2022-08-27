ebd1a14f90c2cb7f03610aa3289a3bb516931d26
FreddyWordingham <freddy@digilab.com>
2022-08-27 23:57:57 +0100

    Added super_sample function

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -13,3 +13,27 @@ pub fn sample(c: Complex, max_iter: i32) -> i32 {
     }
     return max_iter;
 }
+
+fn super_sample(c: Complex, max_iter: i32, super_samples: i32, delta: f64) -> i32 {
+    let start = c + Complex::new(
+        delta * (1 - super_samples) as f64,
+        delta * (1 - super_samples) as f64,
+    );
+    let mut total = 0;
+    for i in 0..super_samples {
+        let re = delta * (i as f64);
+        for j in 0..super_samples {
+            let im = delta * (j as f64);
+            total += sample(start + Complex::new(re, im), max_iter);
+        }
+    }
+    total
+}
+
+fn sample_area() {}
+
+fn data_to_col() {}
+
+fn col_to_image() {}
+
+pub fn image(centre: Complex, scale: f64, res: [usize; 2], super_samples: i32, max_iter: i32) {}
