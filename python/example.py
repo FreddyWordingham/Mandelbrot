8104a61708ac46f158cea4c908c0982c12909871
FreddyWordingham <freddy@digilab.com>
2022-08-28 20:44:24 +0100

    Quality of life changes

--- python/example.py
+++ python/example.py
@@ -1,4 +1,8 @@
 import mandelbrot
+import os
+
+if not os.path.isdir("output"):
+    os.mkdir("output")
 
 centre = mandelbrot.Complex(-0.4605111, 0.56011)
 max_iter = 100
@@ -10,9 +14,7 @@ super_samples = 2
 m = mandelbrot.multi_sample(centre, max_iter, super_samples, 1e-15)
 print(centre, " -> ", m / super_samples**2)
 
-scale = 1e-2
+scale = 1e-0
 res = [1000, 1000]
 # mandelbrot.gpu_render_image(centre, scale, res, super_samples, max_iter, ".")
-mandelbrot.render_video(
-    centre, scale, 0.98, res, 1000, super_samples, max_iter, "output"
-)
+mandelbrot.render_video(centre, scale, 0.9, res, 10, super_samples, max_iter, "output")
